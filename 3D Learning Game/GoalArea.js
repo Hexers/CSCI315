364dbf0c3be5b6837f68cbd1735d1b8038cdb50c
Aleksandar Kljaic <Hexers@users.noreply.github.com>
2019-12-14 09:36:47 -0600

    Create GoalArea.js

---
+++ 3D Learning Game/GoalArea.js
@@ -0,0 +1,10 @@
+#pragma strict
+public static var goal: boolean;
+function Start() {
+goal = false;
+}
+function OnTriggerEnter(col: Collider) {
+if (col.gameObject.tag == "Player") {
+goal = true;
+}
+}
