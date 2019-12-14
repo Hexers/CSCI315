b2389c32f6faf5104744e8c2649c1b969464136b
Aleksandar Kljaic <Hexers@users.noreply.github.com>
2019-12-14 09:39:32 -0600

    Create playerMovement.cs

---
+++ Final Project/playerMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.W))
+		{
+			this.transform.Translate(Vector3.forward * 5.0f * Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.S))
+		{
+			this.transform.Translate(Vector3.back * 5.0f * Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.A))
+		{
+			this.transform.Translate(Vector3.left * 5.0f * Time.deltaTime);
+		}
+		if(Input.GetKey(KeyCode.D))
+		{
+			this.transform.Translate(Vector3.right * 5.0f * Time.deltaTime);
+		}
+		
+	}
+}
