40f67091650887e5b628d40a7658a57f06167b88
Aleksandar Kljaic <Hexers@users.noreply.github.com>
2019-12-14 09:31:27 -0600

    Create main.cpp

---
+++ RandomLoop/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()  // parameters
+{
+
+    for(int val1 = 2; val1 < 10 ; val1++ )
+    {
+        for (int i = 1; i < 10 ; i++ )
+       cout << val1 << " x " << i << " = " << val1 * i << endl;
+    }
+    return 0;
+}
+
+
